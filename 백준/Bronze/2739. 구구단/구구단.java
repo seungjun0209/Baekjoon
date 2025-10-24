4fd95bf9fc84a5647019b473ab72080b2aeb5a76
seungjun0209 <87286979+seungjun0209@users.noreply.github.com>
2025-10-24 22:22:05 +0900

    [Bronze V] Title: 구구단, Time: 172 ms, Memory: 17672 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int a;
+		
+		a = sc.nextInt();
+		
+		for(int i=1;i<=9;i++) {
+			System.out.printf("%d * %d = ",a, i);
+			System.out.println(a*i);
+		}
+
+	}
+
+}
