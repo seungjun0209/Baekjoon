2461851b7cd63e4b5857bd291e33f140df02dafe
seungjun0209 <87286979+seungjun0209@users.noreply.github.com>
2025-10-26 22:54:36 +0900

    [Bronze V] Title: A/B, Time: 168 ms, Memory: 17768 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		double n = sc.nextInt();
+		double m = sc.nextInt();
+
+		System.out.println(n/m);
+
+	}
+
+}
